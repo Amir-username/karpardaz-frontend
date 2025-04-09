c696dba05668333b155dd1ea0088a69da1cc27a8
Amir <najiamirdesktop@gmail.com>
2025-04-09 17:25:29 +0330

    update jobs page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,16 +1,16 @@
-// import AdvertiseList from "@/components/advertise/AdvertiseList"`;
-// import { fetchAdvertisements } from "@/fetch/fetchAdvertisements";
+import AdvertiseList from "@/components/advertise/AdvertiseList";
+import { fetchAdvertisements } from "@/fetch/fetchAdvertisements";
 
 async function Page() {
-  // const jobsData = await fetchAdvertisements();
+  const jobsData = await fetchAdvertisements();
   return (
     <div className="container flex items-center justify-center mx-auto py-12 bg-neutral-light">
       <main className="flex justify-center">
-        {/* {jobsData ? (
+        {jobsData ? (
           <AdvertiseList advertises={jobsData} />
         ) : (
           <div>not found</div>
-        )} */}
+        )}
       </main>
     </div>
   );
