a5ee971e4885c33ab4c8319b613f37d6f558d792
Amir <najiamirdesktop@gmail.com>
2025-04-11 12:42:02 +0330

    update input component for search type

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -23,13 +23,19 @@ function Input({
         }`}
       >
         <input
-          className="w-full h-12 px-4 pl-8 text-sm rounded-lg bg-gray-50 text-neutral-dark placeholder:text-neutral-mid ring-1 ring-gray-300"
+          className={`w-full h-12 px-4 ${
+            type == "search" ? "pr-10" : "pl-8"
+          } text-sm rounded-lg bg-gray-50 text-neutral-dark placeholder:text-neutral-mid ring-1 ring-gray-300`}
           type={type}
           name={name}
           placeholder={placeholder}
         />
         {icon ? (
-          <span className="absolute material-symbols-outlined left-2 top-3 text-neutral-mid">
+          <span
+            className={`absolute material-symbols-outlined ${
+              type == "search" ? "right-2" : "left-2"
+            } top-3 text-neutral-mid`}
+          >
             {icon}
           </span>
         ) : null}
