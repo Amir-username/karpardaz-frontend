6850b3191246345a8162b0847ed283f842d7f86d
Amir <najiamirdesktop@gmail.com>
2025-05-08 16:25:15 +0330

    add employer detail create form

--- src/app/profile/employer/[id]/create/page.tsx
+++ src/app/profile/employer/[id]/create/page.tsx
@@ -1,7 +1,14 @@
-function EmployerCreateDetailPage() {
-    return (
-        <div></div>
-    );
+import EmployerDetailCreateForm from "@/components/employerDetailForm/EmployerDetailCreateForm";
+import { cookies } from "next/headers";
+
+async function EmployerCreateDetailPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token");
+  const role = cookieStore.get("role");
+
+  if (token && role) {
+    return <EmployerDetailCreateForm token={token?.value} role={role?.value} />;
+  }
 }
 
 export default EmployerCreateDetailPage;
---
+++ src/components/employerDetailForm/EmployerDetailCreateForm.tsx
@@ -0,0 +1,106 @@
+"use client";
+import { BASE_LINK } from "@/fetch/config";
+import Button from "@/ui/Button";
+import ClientInput from "@/ui/ClientInput";
+import Form from "@/ui/Form";
+import FormHeader from "@/ui/FormHeader";
+import SelectInput from "@/ui/SelectInput";
+import TextAreaInput from "@/ui/TextAreaInput";
+import { redirect } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+type EmployerDetailCreateFormProps = {
+  role: string;
+  token: string;
+};
+
+function EmployerDetailCreateForm({
+  role,
+  token,
+}: EmployerDetailCreateFormProps) {
+  const [address, setAddress] = useState("");
+  const [population, setPopulation] = useState<
+    "small" | "medium" | "large" | "very large" | string
+  >("medium");
+  const [description, setDescription] = useState("");
+  const [website, setWebsite] = useState("");
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const body = {
+      address: address,
+      population:
+        population === "small"
+          ? "۵ تا ۲۰ نفر"
+          : population === "medium"
+          ? "۲۰ تا ۵۰ نفر"
+          : population === "large"
+          ? "۵۰ تا ۱۰۰ نفر"
+          : "۱۰۰ نفر به بالا",
+      description: description,
+      website: website,
+    };
+
+    const fetchAddEmployerDetail = async (accessToken: string) => {
+      const res = await fetch(BASE_LINK + "employer-detail/", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+    };
+
+    if (role === "employer") {
+      fetchAddEmployerDetail(token);
+      redirect('/')
+    }
+  };
+
+  return (
+    <main className="flex items-center justify-center p-8 mt-28">
+      <Form onFormSubmit={handleFormSubmit}>
+        <FormHeader title="تکمیل پروفایل" subtitle="کارفرما" />
+        <ClientInput
+          placeholder="آدرس کامل سازمان"
+          value={address}
+          setValue={setAddress}
+          icon="apartment"
+        />
+        <ClientInput
+          placeholder="آدرس وب سایت"
+          value={website}
+          setValue={setWebsite}
+          icon="link"
+        />
+        <SelectInput
+          name="population"
+          value={population}
+          setValue={setPopulation}
+          label="جمعیت سازمان"
+        >
+          <option value="small">۵ تا ۲۰ نفر</option>
+          <option value="medium">۲۰ تا ۵۰ نفر</option>
+          <option value="large">۵۰ تا ۱۰۰ نفر</option>
+          <option value="very large">۱۰۰ نفر به بالا</option>
+        </SelectInput>
+        <TextAreaInput
+          label="معرفی سازمان و توضیحات"
+          name="description"
+          value={description}
+          setValue={setDescription}
+        />
+        <Button text="ایجاد پروفایل" type="submit" />
+      </Form>
+    </main>
+  );
+}
+
+export default EmployerDetailCreateForm;
