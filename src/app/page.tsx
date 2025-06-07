95b82577636b5a043712d21a6a7f07af4fac81ca
amir <najiamirdesktop@gmail.com>
2025-06-07 21:05:45 +0330

    add hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import DeveloperILL from "../../public/Illustrations/DeveloperILL.svg";
+
 export default async function Home() {
   return (
-    <div>
-      {/* <Navbar /> */}
-    </div>
+    <main className="w-full flex justify-center">
+      <header className="bg-primary-blue flex gap-8 py-40 items-center text-center justify-center px-64 text-neutral-light">
+        <h1 className="fade-in-right font-lalezar text-6xl leading-20">
+          وبسایت استخدام و کاریابی برای سازمان ها و متخصصان فناوری
+        </h1>
+        <Image
+          className="fade-in-right"
+          width={250}
+          height={250}
+          alt="developer illustration"
+          src={DeveloperILL}
+        />
+      </header>
+    </main>
   );
 }
