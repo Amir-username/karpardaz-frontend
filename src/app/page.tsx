fba2ea4e89d7f90e69246796075e78e8b4ccd0bf
amir <najiamirdesktop@gmail.com>
2025-06-09 20:18:13 +0330

    add carousels for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
 import HeroHeader from "@/components/HeroHeader/HeroHeader";
+import { fetchAdvertisements } from "@/fetch/employerAdvertise/fetchAdvertisements";
+import { AdvertiseModel } from "@/models/Advertise";
 import Carousel, { CarouselItem } from "@/ui/Carousel";
-import Image from "next/image";
 
 export default async function Home() {
+  const adData = await fetchAdvertisements();
+  const advertises: AdvertiseModel[] = adData.advertises;
+
   return (
-    <main className="flex-col flex justify-center">
+    <main className="flex-col gap-4 flex justify-center">
       <HeroHeader />
-      {/* <Carousel header="تازه ترین آگهی ها">
-        <CarouselItem title="برنامه نویس بکند"/>
-        <CarouselItem title="برنامه نویس بکند"/>
-        <CarouselItem title="برنامه نویس بکند"/>
-        <CarouselItem title="برنامه نویس بکند"/>
-        <CarouselItem title="برنامه نویس بکند"/>
-        <CarouselItem title="برنامه نویس بکند"/>
-      </Carousel> */}
+      <Carousel header="تازه ترین آگهی ها">
+        {advertises.map((ad) => {
+          return <CarouselItem key={ad.id} title={ad.title} />;
+        })}
+      </Carousel>
+      <Carousel header="لیست کارفرما">
+        {advertises.map((ad) => {
+          return <CarouselItem key={ad.id} title={ad.title} />;
+        })}
+      </Carousel>
+      <Carousel header="لیست کارجو">
+        {advertises.map((ad) => {
+          return <CarouselItem key={ad.id} title={ad.title} />;
+        })}
+      </Carousel>
     </main>
   );
 }
--- src/ui/Carousel.tsx
+++ src/ui/Carousel.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ImagePoster from "../../public/images/company_default_avatar.png";
 import React from "react";
+import Button from "./Button";
 
 type CarouselProps = {
   header: string;
@@ -9,11 +10,14 @@ type CarouselProps = {
 
 export default function Carousel({ header, children }: CarouselProps) {
   return (
-    // <div className="flex w-full flex-col gap-4 px-8 py-12">
-    //   <header className="flex text-2xl text-primary-blue">{header}</header>
-    //   <ul className="flex py-2 gap-4 flex-shrink">{children}</ul>
-    // </div>
-    <div></div>
+    <div className="flex w-full flex-col gap-4 px-8 py-4">
+      <header className="flex text-xl lg:text-2xl px-1 text-primary-blue">
+        {header}
+      </header>
+      <ul className="flex py-2 px-1 gap-4 overflow-scroll flex-shrink">
+        {children}
+      </ul>
+    </div>
   );
 }
 
@@ -23,9 +27,11 @@ type CarouselItemProps = {
 
 export function CarouselItem({ title }: CarouselItemProps) {
   return (
-    <li className="flex flex-col hover:shadow-lg cursor-pointer items-center px-2 py-4 justify-center ring-1 ring-gray-300 rounded-lg shadow-sm gap-3">
-      <Image src={ImagePoster} alt="sadas" width={150} height={450} />
-      <h3 className="text-xl">{title}</h3>
+    <li className="flex justify-between w-32 h-32 lg:w-48 lg:h-48 flex-shrink-0 flex-col hover:shadow-lg cursor-pointer items-center ring-1 ring-gray-300 rounded-lg shadow-sm gap-3">
+      <h3 className="flex items-center p-2 w-full justify-center h-full text-md text-center lg:text-xl">
+        {title}
+      </h3>
+      <Button h="h-8" card text="مشاهده" />
     </li>
   );
 }
