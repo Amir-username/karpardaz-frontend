702e76e8d2ce149f042336df1376b1603e99577d
Amir <najiamirdesktop@gmail.com>
2025-06-10 15:18:19 +0330

    add animation for carousels

--- src/ui/Carousel.tsx
+++ src/ui/Carousel.tsx
@@ -11,7 +11,7 @@ type CarouselProps = {
 
 export default function Carousel({ header, children }: CarouselProps) {
   return (
-    <div className="flex w-full flex-col gap-4 px-8 py-4">
+    <div className="flex w-full flex-col gap-4 px-8 py-4 fade-in-right">
       <header className="flex text-xl lg:text-2xl px-1 text-primary-blue">
         {header}
       </header>
