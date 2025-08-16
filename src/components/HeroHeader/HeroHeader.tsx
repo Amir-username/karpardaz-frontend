762f8069b204f704944c2b58b7c67d7df561bb11
Amir <najiamirdesktop@gmail.com>
2025-08-16 11:21:05 +0330

    update hero header

--- src/components/HeroHeader/HeroHeader.tsx
+++ src/components/HeroHeader/HeroHeader.tsx
@@ -3,7 +3,7 @@ import DeveloperILL from "../../../public/Illustrations/DeveloperILL.svg";
 
 export default function HeroHeader() {
   return (
-    <header className="flex gradient-background bg-gradient-to-tr from-primary-blue to-slate-500 dark:from-slate-800 dark:to-primary-blue backdrop-blur-3xl  text-neutral-light">
+    <header className="flex h-[700px] gradient-background bg-gradient-to-tr from-primary-blue to-slate-500 dark:from-slate-800 dark:to-primary-blue backdrop-blur-3xl  text-neutral-light">
       <div className="flex flex-col items-center justify-center gap-8 px-8 py-32 text-center lg:flex-row lg:py-40 lg:px-64 fade-out-view ">
         <Image
           className="w-48 h-48 lg:w-[250px] lg:h-[250px]"
