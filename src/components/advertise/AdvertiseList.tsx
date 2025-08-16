6040e0c2fb00f79140766d099df95a53a9ac4648
amir <najiamirdesktop@gmail.com>
2025-08-16 18:06:34 +0330

    update advertise list empty page

--- src/components/advertise/AdvertiseList.tsx
+++ src/components/advertise/AdvertiseList.tsx
@@ -3,6 +3,8 @@
 import { AdvertiseModel } from "@/models/Advertise";
 import AdvertiseItem from "./AdvertiseItem";
 import { useAdvertiseLike } from "@/hooks/useAdvertiseLike";
+import EmptyIll from "../../../public/Illustrations/NoDataILL.svg";
+import Image from "next/image";
 
 type AdvertiseListProps = {
   advertises: AdvertiseModel[];
@@ -15,17 +17,26 @@ function AdvertiseList({ advertises, token, role }: AdvertiseListProps) {
 
   return (
     <ul className="flex flex-col gap-8 justify-center items-center">
-      {advertises.map((ad) => {
-        return (
-          <AdvertiseItem
-            key={ad.id}
-            advertise={ad}
-            isFav={favAdvertises.includes(ad.id)}
-            token={token}
-            role={role}
-          />
-        );
-      })}
+      {advertises.length > 0 ? (
+        advertises.map((ad) => {
+          return (
+            <AdvertiseItem
+              key={ad.id}
+              advertise={ad}
+              isFav={favAdvertises.includes(ad.id)}
+              token={token}
+              role={role}
+            />
+          );
+        })
+      ) : (
+        <div className="flex flex-col gap-8 w-48 h-48 my-12 mb-16">
+          <Image src={EmptyIll} alt="" />
+          <h3 className="text-center text-xl text-neutral-700 dark:text-neutral-light">
+            فعلا آگهی وجود ندارد
+          </h3>
+        </div>
+      )}
     </ul>
   );
 }
--- src/components/jobseekerAdvertise/JobseekerAdList.tsx
+++ src/components/jobseekerAdvertise/JobseekerAdList.tsx
@@ -3,6 +3,8 @@
 import { JobSeekrAdModel } from "@/models/JobSeekerAd";
 import JobSeekerAdItem from "./JobseekerAdItem";
 import { useAdvertiseLike } from "@/hooks/useAdvertiseLike";
+import Image from "next/image";
+import EmptyIll from "../../../public/Illustrations/NoDataILL.svg";
 
 type JobSeekerAdListProps = {
   advertises: JobSeekrAdModel[];
@@ -11,20 +13,33 @@ type JobSeekerAdListProps = {
 };
 
 function JobSeekerAdList({ advertises, token, role }: JobSeekerAdListProps) {
-  const { favAdvertises } = useAdvertiseLike("employer-favorites/", token, role);
+  const { favAdvertises } = useAdvertiseLike(
+    "employer-favorites/",
+    token,
+    role
+  );
   return (
-    <ul className="flex flex-col gap-8">
-      {advertises.map((ad) => {
-        return (
-          <JobSeekerAdItem
-            key={ad.id}
-            advertise={ad}
-            token={token}
-            role={role}
-            isFav={favAdvertises.includes(ad.id)}
-          />
-        );
-      })}
+    <ul className="flex flex-col gap-8 justify-center items-center">
+      {advertises.length > 0 ? (
+        advertises.map((ad) => {
+          return (
+            <JobSeekerAdItem
+              key={ad.id}
+              advertise={ad}
+              token={token}
+              role={role}
+              isFav={favAdvertises.includes(ad.id)}
+            />
+          );
+        })
+      ) : (
+        <div className="flex flex-col gap-8 w-48 h-48 my-12 mb-16 justify-center items-center">
+          <Image src={EmptyIll} alt="" />
+          <h3 className="text-center text-xl text-neutral-700 dark:text-neutral-light">
+            فعلا آگهی وجود ندارد
+          </h3>
+        </div>
+      )}
     </ul>
   );
 }
